61ffed3c37ffb5f05a666734c8a060413c673cae
StanleyRiche <st.saintima@duocuc.cl>
2023-04-01 16:59:53 -0300

    upload

--- javascript/app.js
+++ javascript/app.js
@@ -1,49 +1,78 @@
 document.addEventListener("DOMContentLoaded",function(){
 
     eventListeners();
-
-    eventListenersuser();
- 
+    
 });
 
-function eventListeners(){
-const mobileMenu =document.querySelector(".mobile-menu");
 
-mobileMenu.addEventListener("click" ,navegacionResponsive);
+function eventListeners(){
+    const mobileMenu =document.querySelector(".mobile-menu");
+
+    mobileMenu.addEventListener("click" ,navegacionResponsive);
 
 }
 
 function navegacionResponsive(){
-    const navegacion= document.querySelector(".navegacion");
+        const navegacion= document.querySelector(".navegacion");
 
-    if(navegacion.classList.contains("mostrar")){
-        navegacion.classList.remove("mostrar");
-    }else{
-        navegacion.classList.add("mostrar");
-    }
-    // O navegacion.classList.toggle("mostrar");
+        if(navegacion.classList.contains("mostrar")){
+            navegacion.classList.remove("mostrar");
+        }else{
+            navegacion.classList.add("mostrar");
+        }
+        // O navegacion.classList.toggle("mostrar");
 }
 
 
+// Funcion de carrusel
 
-
-
-
-
-function eventListenersuser(){
-    const mobileMenu =document.querySelector(".usuario");
-    
-    mobileMenu.addEventListener("click" ,navegacionResponsiveuser);
-    
-}
-    
-function navegacionResponsiveuser(){
-    const navegacionusuario= document.querySelector(".navegacionusuario");
-
-    if(navegacionusuario.classList.contains("mostrar-usuario")){
-        navegacionusuario.classList.remove("mostrar-usuario");
-    }else{
-        navegacionusuario.classList.add("mostrar-usuario");
-    }
-    // O navegacion.classList.toggle("mostrar");
-}
+if(document.querySelector('#container-slider')){
+    setInterval('fntExecuteSlide("next")',5000);
+ }
+ //------------------------------ LIST SLIDER -------------------------
+ if(document.querySelector('.listslider')){
+    let link = document.querySelectorAll(".listslider li a");
+    link.forEach(function(link) {
+       link.addEventListener('click', function(e){
+          e.preventDefault();
+          let item = this.getAttribute('itlist');
+          let arrItem = item.split("_");
+          fntExecuteSlide(arrItem[1]);
+          return false;
+       });
+     });
+ }
+ 
+ function fntExecuteSlide(side){
+     let parentTarget = document.getElementById('slider');
+     let elements = parentTarget.getElementsByTagName('li');
+     let curElement, nextElement;
+ 
+     for(var i=0; i<elements.length;i++){
+ 
+         if(elements[i].style.opacity==1){
+             curElement = i;
+             break;
+         }
+     }
+     if(side == 'prev' || side == 'next'){
+ 
+         if(side=="prev"){
+             nextElement = (curElement == 0)?elements.length -1:curElement -1;
+         }else{
+             nextElement = (curElement == elements.length -1)?0:curElement +1;
+         }
+     }else{
+         nextElement = side;
+         side = (curElement > nextElement)?'prev':'next';
+ 
+     }
+     //RESALTA LOS PUNTOS
+     let elementSel = document.getElementsByClassName("listslider")[0].getElementsByTagName("a");
+     elementSel[curElement].classList.remove("item-select-slid");
+     elementSel[nextElement].classList.add("item-select-slid");
+     elements[curElement].style.opacity=0;
+     elements[curElement].style.zIndex =0;
+     elements[nextElement].style.opacity=1;
+     elements[nextElement].style.zIndex =1;
+ }
