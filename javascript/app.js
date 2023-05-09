03746b5eb8fadeeb296766e4a5fd0dcef8519e66
Padrinosky <128003032+Padrinosky@users.noreply.github.com>
2023-05-09 00:10:53 -0400

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -79,31 +79,77 @@ if(document.querySelector('#container-slider')){
 
 
 
+ const form = document.getElementById('form');
+ const nombre = document.getElementById('nombre');
+ const telefono = document.getElementById('telefono');
+ const email = document.getElementById('email');
+ const mensaje = document.getElementById('mensaje');
 
- 
+ form.addEventListener('submit', e =>{
+    e.preventDefault();
 
- function validarFormulario() {
-    var nombre = document.getElementById("nombre").value;
-    var telefono = document.getElementById("telefono").value;
-    var email = document.getElementById("email").value;
-    var mensaje = document.getElementById("mensaje").value;
-    if (nombre == "") {
-        alert("Por favor, ingrese su nombre.");
-        return false;
+    validateInputs();
+ });
+
+ const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+ }
+
+ const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+ };
+
+ const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+ }
+
+
+ const validateInputs = () => {
+    const nameValue = nombre.value.trim();
+    const telefonoValue = telefono.value.trim();
+    const emailValue = email.value.trim();
+    const mensajeValue = mensaje.value.trim();
+
+    if (nameValue === ''){
+        setError(nombre, 'Ingrese su nombre');
+    } else {
+        setSuccess(nombre);
     }
-    if (telefono == "") {
-        alert("Por favor, ingrese su número de teléfono.");
-        return false;
+
+    if(telefonoValue === '') {
+        setError(telefono, 'Ingrese su numero');
+    } else if (telefonoValue.length < 9 ) {
+        setError(telefono, 'Su número debe contener al menos 9 digitos')
+    } else {
+        setSuccess(telefono);
     }
-    if (email == "") {
-        alert("Por favor, ingrese su dirección de correo.");
-        return false;
+
+    if(emailValue === '') {
+        setError(email, 'Ingrese un email');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Ingrese un email valido');
+    } else {
+        setSuccess(email);
     }
-    if (mensaje == "") {
-        alert("Por favor, ingrese su mensaje.");
-        return false;
+
+    if (mensajeValue === ''){
+        setError(mensaje, 'Ingrese su mensaje');
+    } else if(mensajeValue.length < 25){
+        setError(mensaje, 'su mensaje debe contener al menos 25 caracteres')
+    } else{    
+        setSuccess(mensaje);
     }
-    return true;
-}
+
+ };
 
   
